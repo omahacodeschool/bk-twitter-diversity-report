fa98b518709f76034b70dafa965e27bc8805c611
Tony Plaza <tplaza3@gmail.com>
2014-10-13 15:44:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterDiversityReport::Application.config.secret_token = '5d6892c8bf8e6b67a01cddfb3aba6ed88d511158430f3b0848f6b8142ced4f2c77fe222ae06648957cce7307def276fb661c32cfb8a1eb66b107267e479c2780'
