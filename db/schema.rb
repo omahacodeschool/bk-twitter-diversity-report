4ed0e9db2e1f29a17fc8923049f2b0e79a80a7b0
Tony Plaza <tplaza3@gmail.com>
2014-10-14 10:53:52 -0500

    update users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141013235435) do
+ActiveRecord::Schema.define(:version => 20141014154807) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20141013235435) do
     t.string   "race_array"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "ages_array"
   end
 
 end
