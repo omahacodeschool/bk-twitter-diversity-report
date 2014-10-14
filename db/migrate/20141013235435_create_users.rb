f2ccb74303788de35d0612f02a73f9c0defe1b1c
Tony Plaza <tplaza3@gmail.com>
2014-10-14 10:37:51 -0500

    run user migration

--- db/migrate/20141013235435_create_users.rb
+++ db/migrate/20141013235435_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :gender_array
-      t.integer, :age_array
+      t.integer :age_array
       t.string :race_array
 
       t.timestamps
