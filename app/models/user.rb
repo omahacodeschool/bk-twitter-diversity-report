650087bfcf48d85a5654406bde68b402525c93f1
“Jason <j.lukasson@gmail.com>
2014-10-14 21:49:00 -0500

    commiting omniauth sign in/out changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,17 @@ class User < ActiveRecord::Base
     config.access_token_secret = ENV['ACCESS_SECRET']
   end
 
-  binding.pry
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+  
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+    end
+  end
   
   def string_to_array(string)
     string.split(" ")
